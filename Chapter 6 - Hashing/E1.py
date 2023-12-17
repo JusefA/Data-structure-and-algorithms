7c976ee9d9d53544e4a9914fed779b4214b61845
Jusef <67407501+JusefA@users.noreply.github.com>
2023-12-17 13:18:17 +0200

    Create E1.py

---
+++ Chapter 6 - Hashing/E1.py
@@ -0,0 +1,51 @@
+class HashItem():
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+    
+    def __repr__(self):
+        return f'{{{self.key}: {self.value}}}'
+
+class HashTable():
+    def __init__(self, size=256):
+        self.size = size
+        self.slots = [None] * size
+        self.used_slots = 0
+    
+    def __repr__(self):
+        text = ''
+        for index, slot in enumerate(self.slots):
+            if slot:
+                text += f', {index}: {slot}'
+        plural = '' if self.used_slots == 1 else 's'
+        return f'<HashTable ({self.used_slots} element{plural}): [{text.lstrip(", ")}]'
+
+    def _hash(self, key):
+        """
+        Hashing function. Can be changed for a custom one.
+        """
+        return len(key) % self.size
+
+    def _find_free_slot(self, start):
+        """
+        Starting from 'start' find the next free slot available.
+        
+        Parameters:
+        - 'start': Starting point for the search.
+
+        Returns: The index of the next free slot or None if no free slots
+        """
+        current_index = start
+
+        while True:
+            # Check if the current slot is empty (None)
+            if self.slots[current_index] is None:
+                return current_index
+
+            # Move to the next index
+            current_index = (current_index + 1) % self.size
+
+            # If we have iterated through the entire table and reached the starting point again
+            # without finding a free slot, return None to avoid an infinite loop
+            if current_index == start:
+                return None
